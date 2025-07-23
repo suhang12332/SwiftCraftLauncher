a9657ac5fd4bdb591c107770528bea1aff1c4960
弟中弟 <1552899301@qq.com>
2025-07-23 23:39:36 +0800

    Initial Commit

---
+++ SwiftCraftLauncher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftCraftLauncher
+//
+//  Created by su on 2025/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftCraftLauncher/SwiftCraftLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCraftLauncherApp.swift
+//  SwiftCraftLauncher
+//
+//  Created by su on 2025/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCraftLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
